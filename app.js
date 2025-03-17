b72e21c436feb96d7c12e67d2d506002f6458afb
Monica Ojeda <monica2519@hotmail.com>
2025-03-17 19:32:14 -0300

    Funciones, agregar amigo, actualizar, sortear

--- app.js
+++ app.js
@@ -1,2 +1,44 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []
+const amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+
+    actualizarListaAmigos();
+
+    input.value = "";
+}
+
+function actualizarListaAmigos() {
+
+    const listaAmigos = document.getElementById("listaAmigos");
+
+    listaAmigos.innerHTML = "";
+
+
+    for (let i = 0; i < amigos.length; i++) {
+        listaAmigos.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+
+function sortearAmigo() {
+
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultadoElemento = document.getElementById("resultado");
+    resultadoElemento.innerHTML = `<li>${amigoSorteado}</li>`;
+}
